08cadb5abb36b7c86ef92e3d80aaa2ba3cd4a1d6
classy-dev <classy-dev@users.noreply.github.com>
2020-09-24 15:56:43 +0200

    fix

--- app/components/Chat/ConversationList/index.js
+++ app/components/Chat/ConversationList/index.js
@@ -17,7 +17,7 @@ export default function ConversationList({ handleClickRoom }) {
     const placeholderImagePath = '/images/avatars/pp_boy.svg';
     axios({
       method: 'POST',
-      url: `http://portal.keukenvergelijking.nl/api/${pluralUserType}/getRooms`,
+      url: `https://portal.keukenvergelijking.nl/api/${pluralUserType}/getRooms`,
       headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
     })
       .then((res) => {
